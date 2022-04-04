8181fef213387170046b9fbe97bc6e781c045a3b
Serhii Litviachenko <slitviachenko@gmail.com>
2022-04-03 22:43:25 -0500

    created src/lib/templateMetadata.js

--- src/commands/check.js
+++ src/commands/check.js
@@ -10,42 +10,26 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const exec = require('sync-exec')
-
-// get metadata about NPM packages using npm view command
-async function getTemplateMetadata(packageName) {
-    // Exec output contains both stderr and stdout outputs
-    return exec(`npm view ${packageName} --json`).stdout
-}
-
-function getChecks() {
-    // import individual checks
-    const checkNodeVersion = require('../checks/checkNodeVersion')
-    const descriptionNotEmpty = require('../checks/descriptionNotEmpty')
-    const containsKeyword = require('../checks/containsKeyword')
-    return [checkNodeVersion, descriptionNotEmpty, containsKeyword]
-}
+const { checkTemplateMetadata } = require('../lib/templateMetadata')
 
 function outputResults(results, outputInJson) {
     if (outputInJson === true) {
-        results.stats.failures !== 0
-            ? console.error(JSON.stringify(results, null, 4))
-            : console.log(JSON.stringify(results, null, 4))
+        console.log(JSON.stringify(results, null, 4))
     } else {
         if (results.stats.failures !== 0) {
-            console.error(
+            console.log(
                 `${results.stats.failures} of ${results.stats.tests} tests failed.`
             )
-            console.error('Failed tests:')
+            console.log('Failed tests:')
             let i = 1
             for (const test of results.failures) {
-                console.error(` ${i++}) ${test.description}`)
-                console.error(`    Error: ${test.message}`)
+                console.log(` ${i++}) ${test.description}`)
+                console.log(`    Error: ${test.message}`)
             }
-            console.error('Passed tests:')
+            console.log('Passed tests:')
             i = 1
             for (const test of results.passes) {
-                console.error(` ${i++}) ${test.description}`)
+                console.log(` ${i++}) ${test.description}`)
             }
         } else {
             console.log(
@@ -66,32 +50,7 @@ async function check(packageUrl, options) {
     // parse package URL input for package name
     let p_url = new URL(packageUrl)
     let packageName = p_url.pathname.split('package/')[1]
-    // get metadata using package name and convert to JSON object
-    let templateMetadata = await getTemplateMetadata(packageName)
-    let templateJSON = JSON.parse(templateMetadata)
-    let results = {
-        stats: {
-            tests: 0,
-            passes: 0,
-            failures: 0,
-        },
-        passes: [],
-        failures: [],
-    }
-    // run all checks on metadata JSON object
-    const checks = getChecks()
-    for (const check of checks) {
-        let result = await check.method(templateJSON)
-        results.stats.tests++
-        result = { ...{ description: check.description }, ...result }
-        if (result.status === 'fail') {
-            results.stats.failures++
-            results.failures.push(result)
-        } else {
-            results.stats.passes++
-            results.passes.push(result)
-        }
-    }
+    let results = await checkTemplateMetadata(packageName)
     process.exitCode = results.stats.failures !== 0 ? 1 : 0
     outputResults(results, outputInJson)
 }
